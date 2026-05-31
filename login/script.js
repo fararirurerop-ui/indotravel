32d9c2f888d8463e8b90a736405bccc4cb77531f
fararirurerop-ui <fara.rirurerop@gmail.com>
2026-05-31 23:59:59 +0700

    Create script.js

---
+++ login/script.js
@@ -0,0 +1,21 @@
+var btnLogin = document.getElementById("do-login");
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+
+btnLogin.onclick = function () {
+  
+  if (username.value === "heri" && password.value === "123") {
+    
+    localStorage.setItem("loggedIn", "true");
+    localStorage.setItem("user", username.value) ;
+    
+    alert("Login berhasil!");
+    
+    // balik ke halaman utama
+    window.location.href = "../index.html";
+
+  } else {
+    alert("Username atau Password salah!");
+  }
+
+};
