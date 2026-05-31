2d3d7fa1bb0f79ad55e08e37b0f0384c46188036
fararirurerop-ui <fara.rirurerop@gmail.com>
2026-05-31 23:35:14 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -2,20 +2,20 @@ var btnLogin = document.getElementById("do-login");
 var username = document.getElementById("username");
 var password = document.getElementById("password");
 
-btnLogin.onclick= function () {
+btnLogin.onclick = function () {
     
-if (username. value === "heri" && password. value === "123") {
+    if (username.value === "heri" && password.value === "123") {
     
-    localStorage.setItem("loggedIn", "true");
-    localStorage. setItem("user", username. value) ;
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("user", username.value) ;
     
-    alert ("Login berhasil!");
+        alert ("Login berhasil!");
     
-    // balik ke halaman utama
-    window.location.href ="../index.html";
+        // balik ke halaman utama
+        window.location.href = "../index.html";
     
-} else {
-    alert ("Username atau Password salah!");
-} 
+    } else {
+        alert("Username atau Password salah!");
+    } 
    
 });
