33651c123ecc9f5537d69561310053cdde7adad0
fararirurerop-ui <fara.rirurerop@gmail.com>
2026-05-31 23:23:19 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,36 +1,21 @@
-document.getElementById("loginForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
+var btnLogin = document.getElementById("do-login");
+var username = document.getElementById("username");
+var password = document.getElementById("password");
 
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-
-    const res = await fetch("https://fararirurerop-ui.github.io/indotravel/login/index.html", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-    });
-
-    const data = await res.json();
-
-    if (data.status === "success") {
-        // simpan username
-            localStorage.setItem("username", data.username);
-            window.location.href = "../index.html";
-         
-    // } else {
-    //     document.getElementById("message").innerText = "Username / Password salah";alert("Login gagal");
-    // }
+btnLogin.onclick= function () {
     
-    } else {
-    const alertBox = document.getElementById("alertBox");
-    alertBox.innerText = "Username atau Password salah, silahkan coba lagi";
-    alertBox.style.display = "block";
-
-    setTimeout(() => {
-        alertBox.style.display = "none";
-    }, 3000);
+if (username. value === "heri" && password. value === "123") {
+    
+    localStorage.setItem("loggedIn", "true");
+    localStorage. setItem("user", username. value) ;
+    
+    alert ("Login berhasil!");
+    
+    // balik ke halaman utama
+    window.location.href ="../index.html";
+    
+} else {
+    alert ("Username atau Password salah!");
 } 
    
 });
