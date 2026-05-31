e7b410a0b9066a6ab550ce1d6f5a38c57adc1d70
fararirurerop-ui <fara.rirurerop@gmail.com>
2026-06-01 00:07:54 +0700

    Update script.js

--- login/script.js
+++ login/script.js
@@ -2,20 +2,22 @@ var btnLogin = document.getElementById("do-login");
 var username = document.getElementById("username");
 var password = document.getElementById("password");
 
-btnLogin.onclick = function () {
-  
-  if (username.value === "heri" && password.value === "123") {
-    
-    localStorage.setItem("loggedIn", "true");
-    localStorage.setItem("user", username.value) ;
-    
-    alert("Login berhasil!");
-    
-    // balik ke halaman utama
-    window.location.href = "../index.html";
+btnLogin.addEventListener("click", function () {
 
-  } else {
-    alert("Username atau Password salah!");
-  }
+    if (username.value === "heri" && password.value === "123") {
 
-};
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("user", username.value);
+
+        alert("Login berhasil!");
+
+        // redirect ke halaman utama GitHub Pages
+        window.location.href = "https://fararirurerop-ui.github.io/indotravel/";
+
+    } else {
+
+        alert("Username atau Password salah!");
+
+    }
+
+});
