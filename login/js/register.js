ebb4aa7fd7a1b9dd2952572ce9b14a97963c736f
fararirurerop-ui <fara.rirurerop@gmail.com>
2026-06-07 12:39:30 +0700

    Update register.js

--- login/js/register.js
+++ login/js/register.js
@@ -1,24 +1,34 @@
-document.getElementById("registerForm").addEventListener("submit", async function(e) {
+document
+.getElementById("registerForm")
+.addEventListener("submit", function(e){
+
     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value.trim();
+    const username =
+    document.getElementById("username").value;
 
-    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=register&username=${encodeURIComponent(username)}&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
-    });
 
-    const data = await res.json();
+    const password =
+    document.getElementById("password").value;
 
-    if (data.status === "success") {
-        document.getElementById("message").innerText = "Registrasi berhasil, silakan login";
-        window.location.href = "index.html";
-    } else {
-        document.getElementById("message").innerText = data.message || "Gagal registrasi";
+    if(localStorage.getItem(username)){
+
+        alert("Username sudah digunakan");
+        return;
     }
+
+    const user = {
+        username,
+        password
+    };
+
+    localStorage.setItem(
+        username,
+        JSON.stringify(user)
+    );
+
+    alert("Registrasi berhasil");
+
+    window.location.href = "index.html";
+
 });
