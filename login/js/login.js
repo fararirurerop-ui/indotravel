e4bfaf4b8534270b053eb14dd3663baa925b1531
fararirurerop-ui <fara.rirurerop@gmail.com>
2026-06-07 14:17:21 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,16 +1,3 @@
-document.getElementById("loginForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
-
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-
-    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-    });
 
 var btnLogin = document.getElementById("do-login");
 var username = document.getElementById("username");
