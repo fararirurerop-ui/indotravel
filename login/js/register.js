b767e72d534546e9947210c6f9274aedc7c997e4
fararirurerop-ui <fara.rirurerop@gmail.com>
2026-06-07 14:19:56 +0700

    Update register.js

--- login/js/register.js
+++ login/js/register.js
@@ -1,17 +1,6 @@
-document.getElementById("registerForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
-
-    const username = document.getElementById("username").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value.trim();
-
-    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=register&username=${encodeURIComponent(username)}&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
-    });
+var btnLogin = document.getElementById("do-daftar");
+var username = document.getElementById("username");
+var password = document.getElementById("password");
 
     const data = await res.json();
 
